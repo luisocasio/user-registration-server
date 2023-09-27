7db11c6cd71a41ada371751f4af7417be3592292
luisocasio <luopscode@gmail.com>
2023-09-27 14:27:05 -0600

    add urls

--- src/api/server.js
+++ src/api/server.js
@@ -15,7 +15,10 @@ server.use(express.json());
 server.use(cookieParser());
 server.use(cors({
   credentials: true, 
-  origin: ['http://localhost:3001'
+  origin: ['http://localhost:3001',
+  'https://user-registration-app-six.vercel.app',
+  'https://user-registration-app-six.vercel.app/login',
+  'https://user-registration-app-six.vercel.app/user/login'
 ]}));
 server.use(morgan("tiny"));
 
